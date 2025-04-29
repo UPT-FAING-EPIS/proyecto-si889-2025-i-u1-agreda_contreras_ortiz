957adf0cdcf040cf8328e14e90a5cf95f1c971db
ximena-ortiz <xo2021071080@virtual.upt.pe>
2025-04-28 21:54:36 -0500

    #20 Infra

--- infra/main.tf
+++ infra/main.tf
@@ -9,12 +9,10 @@ resource "azurerm_app_service_plan" "asp" {
   resource_group_name = azurerm_resource_group.rg.name
 
   sku {
-    tier = "PremiumV2"
-    size = "P1v2"
+    tier = "Free"
+    size = "F1"
   }
 
-  kind = "Linux"
-  reserved = true
 }
 
 resource "azurerm_linux_web_app" "webapps" {
@@ -22,14 +20,9 @@ resource "azurerm_linux_web_app" "webapps" {
   name                = each.value.name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_app_service_plan.asp.id
+  service_plan_id = azurerm_app_service_plan.asp.id
 
   site_config {
-    always_on = true
-    linux_fx_version = "NODE|18-lts"
-  }
-
-  app_settings = {
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+    always_on = false  # Siempre en false en capa gratuita porque "always_on" solo se permite en planes pagos
   }
 }
