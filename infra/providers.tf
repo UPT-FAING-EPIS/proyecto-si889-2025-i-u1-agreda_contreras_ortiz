e8e919a648d35553ffba3f5fa40d5613206ed94d
ximena-ortiz <102675516+ximena-ortiz@users.noreply.github.com>
2025-05-01 02:37:59 -0500

    #20 providers.tf

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
-
   required_version = ">= 1.0"
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = "1e10e2dc-a5f5-43ec-95f2-b0b41a864f67"
 }
